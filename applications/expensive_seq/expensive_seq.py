db54af4fe5686c795d001d60a4195a9a0afd46d8
evan <aspaasevan@gmail.com>
2020-07-09 11:11:02 -0700

    expensive seq tests passing

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,27 @@
 # Your code here
+import math
+import random
 
+cache = {}
 
 def expensive_seq(x, y, z):
     # Your code here
+    global cache 
 
+    if x <= 0:
+        return y + z
+    elif (x, y, z) in cache:
+        return cache[(x, y, z)]
+    else:
+        cache[(x, y, z)] = expensive_seq(x-1, y+1, z) \
+            + expensive_seq(x - 2, y + 2, z * 2) \
+            + expensive_seq(x - 3, y + 3, z * 3)
+        return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+        x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
